5d8a481566f536e7cb37e7784bfc3b155536c87a
kasperisager <kasperisager@gmail.com>
2014-01-24 15:55:54 -0500

    Lint source code

--- default.php
+++ default.php
@@ -1,35 +1,35 @@
 <?php if (!defined('APPLICATION')) exit();
 
-$PluginInfo['OEmbed'] = array(
-   'Name'         => 'OEmbed',
-   'Description'  => 'OEmbed enables you to embed content from a myriad of different providers. Now with Spotify support!',
-   'Version'      => '1.3.1',
-   'Author'       => 'Kasper K. Isager',
-   'AuthorEmail'  => 'kasperisager@gmail.com',
-   'AuthorUrl'    => 'http://github.com/kasperisager'
+$PluginInfo['oembed'] = array(
+    'Name'        => 'OEmbed',
+    'Description' => 'OEmbed enables you to embed content from a myriad of different providers. Now with Spotify support!',
+    'Version'     => '1.3.1',
+    'Author'      => 'Kasper K. Isager',
+    'AuthorEmail' => 'kasperisager@gmail.com',
+    'AuthorUrl'   => 'http://github.com/kasperisager'
 );
 
 /**
  * OEmbed plugin for Vanilla
  *
- * @since      1.0
- * @author     Kasper Kronborg Isager <kasperisager@gmail.com>
- * @copyright  Copyright © 2013
- * @license    http://opensource.org/licenses/MIT MIT
+ * @since     1.0
+ * @author    Kasper Kronborg Isager <kasperisager@gmail.com>
+ * @copyright Copyright © 2013
+ * @license   http://opensource.org/licenses/MIT MIT
  */
 class OEmbed extends Gdn_Plugin
 {
 	/**
-    * Add OEmbed to discussion pages
-    *
-    * @param  Gdn_Controller $Sender
-    * @since  1.0
-    * @access public
-    */
-   public function DiscussionController_Render_Before($Sender)
-   {
-      $Sender->AddCssFile('oembed.css', 'plugins/OEmbed');
-      $Sender->AddJsFile('jquery.oembed.js', 'plugins/OEmbed');
-      $Sender->AddJsFile('jquery.providers.js', 'plugins/OEmbed');
-   }
+     * Add OEmbed to discussion pages
+     *
+     * @since  1.0
+     * @param  DiscussionController $Sender
+     * @access public
+     */
+    public function DiscussionController_Render_Before($Sender)
+    {
+        $Sender->AddCssFile('oembed.css', 'plugins/OEmbed');
+        $Sender->AddJsFile('jquery.oembed.js', 'plugins/OEmbed');
+        $Sender->AddJsFile('jquery.providers.js', 'plugins/OEmbed');
+    }
 }
